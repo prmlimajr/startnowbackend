412caf4ff3d33bae9579a4320e71d24183d11435
Paulo Lima <prmlimajr@hotmail.com>
2020-11-14 19:15:00 -0300

    Knowledges

--- src/app/controllers/KnowledgeController.js
+++ src/app/controllers/KnowledgeController.js
@@ -51,6 +51,57 @@ class KnowledgeController {
       ...knowledges,
     });
   }
+
+  async update(req, res) {
+    Logger.header('controller - knowledge - update');
+
+    const { marketing, finances, legislation, leadership, sales } = req.body;
+
+    Logger.log(
+      `[${marketing}][${finances}][${legislation}][${leadership}][${sales}]`
+    );
+
+    const schema = Yup.object().shape({
+      marketing: Yup.number().positive(),
+      finances: Yup.number().positive(),
+      legislation: Yup.number().positive(),
+      leadership: Yup.number().positive(),
+      sales: Yup.number().positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      Logger.error('Validation failed');
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const [userExists] = await connection('user')
+      .select('user.*')
+      .where('user.id', '=', req.userId);
+
+    if (!userExists) {
+      Logger.error('User not found');
+
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    const knowledges = {
+      userId: req.userId,
+      marketing: marketing || userExists.marketing,
+      finances: finances || userExists.finances,
+      legislation: legislation || userExists.legislation,
+      leadership: leadership || userExists.leadership,
+      sales: sales || userExists.sales,
+      created: userExists.created,
+      updated: new Date(),
+    };
+
+    await connection('knowledge')
+      .update(knowledges)
+      .where('knowledge.userId', '=', req.userId);
+
+    Logger.success('[200]');
+    return res.json(knowledges);
+  }
 }
 
 module.exports = new KnowledgeController();
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const SessionController = require('./app/controllers/SessionController');
 const RankingController = require('./app/controllers/RankingController');
 
 const authMiddleware = require('./app/middlewares/auth');
+const KnowledgeController = require('./app/controllers/KnowledgeController');
 
 const routes = new Router();
 
@@ -15,6 +16,9 @@ routes.post('/sessions', SessionController.store);
 routes.get('/ranking', RankingController.list);
 
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+routes.post('/knowledges', KnowledgeController.create);
+routes.put('/knowledges', KnowledgeController.update);
 
 module.exports = routes;
